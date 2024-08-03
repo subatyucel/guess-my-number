352d23f21a90ac895e04bff110a331913dcda762
SubatYucel <subat.yucel1@gmail.com>
2024-08-03 18:37:29 +0300

    again button functionality added

--- script.js
+++ script.js
@@ -3,32 +3,58 @@
 const guess = document.querySelector('.guess'); //input
 const message = document.querySelector('.message'); //message
 const checkBtn = document.querySelector('.check'); //check button
-const randomNumber = Math.floor(Math.random() * 20) + 1; //the random number that will be guessed
+let randomNumber = Math.floor(Math.random() * 20) + 1; //the random number that will be guessed
 const score = document.querySelector('.score'); //the score
 let scoreCounter = 20; //the score's text
-document.querySelector('.number').textContent = randomNumber;
+const body = document.querySelector('body'); //body element
+const number = document.querySelector('.number'); //number on the screen "?"
+const againBtn = document.querySelector('.again'); //again button
 
+//checks if you guessed correctly
 const check = function () {
   let guessed = Number(guess.value);
+  //if the user have points to guess
   if (scoreCounter > 1) {
+    //if no input
     if (!guessed) {
       message.textContent = '🚫 No number!';
-    } else if (guessed === randomNumber) {
+    }
+    //if user wins
+    else if (guessed === randomNumber) {
       message.textContent = '🎉 Correct!';
-    } else if (guessed < randomNumber) {
+      body.style.background = '#60b347';
+      number.textContent = randomNumber;
+    }
+    //if user guesses low
+    else if (guessed < randomNumber) {
       message.textContent = '📉 Too low!';
       scoreCounter--;
       score.textContent = scoreCounter;
-    } else {
+    }
+    //if user guesses high
+    else {
       message.textContent = '📈 Too high!';
       scoreCounter--;
       score.textContent = scoreCounter;
     }
-  } else {
+  }
+  //if the user run out of points
+  else {
     scoreCounter--;
     score.textContent = 0;
     message.textContent = 'You lost 😭';
   }
 };
 
+//resets the game
+const playAgain = function () {
+  message.textContent = 'Start guessing...';
+  scoreCounter = 20;
+  body.style.background = '#222';
+  guess.value = '';
+  number.textContent = '?';
+  randomNumber = Math.floor(Math.random() * 20) + 1;
+};
+
 checkBtn.addEventListener('click', check);
+againBtn.addEventListener('click', playAgain);
